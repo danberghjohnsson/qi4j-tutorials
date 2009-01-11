f4b8edbe5460a960a5ab945d2746af5096f21866
niclas@hedhman.org <niclas@hedhman.org@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-01-11 11:25:28 +0000

    o Moving 'org.qi4j.entity.index.rdf' to 'org.qi4j.index.rdf'
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/tutorials@13972 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- recipe/src/main/java/org/qi4j/tutorials/recipe/assembly/InfrastructureAssembler.java
+++ recipe/src/main/java/org/qi4j/tutorials/recipe/assembly/InfrastructureAssembler.java
@@ -18,7 +18,7 @@ import org.qi4j.bootstrap.Assembler;
 import org.qi4j.bootstrap.AssemblyException;
 import org.qi4j.bootstrap.ModuleAssembly;
 import org.qi4j.entitystore.memory.MemoryEntityStoreService;
-import org.qi4j.entity.index.rdf.assembly.RdfMemoryStoreAssembler;
+import org.qi4j.index.rdf.assembly.RdfMemoryStoreAssembler;
 import org.qi4j.spi.entity.helpers.UuidIdentityGeneratorService;
 import org.qi4j.api.common.Visibility;
 
