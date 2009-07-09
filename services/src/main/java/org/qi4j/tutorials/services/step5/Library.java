9f353e430d5641b8fb5f40613f7f9f59f6c5afec
Niclas Hedhman <niclas@hedhman.org>
2009-07-09 16:41:40 +0800

    Fixing up a Service tutorials.

---
+++ services/src/main/java/org/qi4j/tutorials/services/step5/Library.java
@@ -0,0 +1,10 @@
+package org.qi4j.tutorials.services.step5;
+
+
+
+public interface Library
+{
+    Book borrowBook( String author, String title );
+
+    void returnBook( Book book );
+}
---
+++ services/src/main/java/org/qi4j/tutorials/services/step6/Library.java
@@ -0,0 +1,9 @@
+package org.qi4j.tutorials.services.step6;
+
+
+public interface Library
+{
+    Book borrowBook( String author, String title );
+
+    void returnBook( Book book );
+}
